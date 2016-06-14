57f08f027b3e0112a493f4916d7a5cf37ba9ce45
FranciZ <franci@proxima.si>
2016-06-14 19:29:35 +0200

    feature: stream motion data;

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,31 @@ var app = {
 
     token:null,
     socket:null,
+    detectMotion:function(){
+
+        window.addEventListener('devicemotion', function(event) {
+            var x = event.acceleration.x;
+            var y = event.acceleration.y;
+            var z = event.acceleration.z;
+
+            var ralpha = event.rotationRate.alpha;
+            var rbeta = event.rotationRate.beta;
+            var rgamma = event.rotationRate.gamma;
+
+            var interval = event.interval;
+
+            var $row1 = $('<p>',{ text:'x: '+x });
+            var $row2 = $('<p>',{ text:'y: '+y });
+            var $row3 = $('<p>',{ text:'z: '+z });
+            var $row4 = $('<p>',{ text:'ralpha: '+ralpha });
+            var $row5 = $('<p>',{ text:'rbeta: '+rbeta });
+            var $row6 = $('<p>',{ text:'rgamma: '+rgamma });
+
+            $('#motion-log').prepend($row1, $row2, $row3, $row4, $row5, $row6);
+
+        });
+
+    },
     connectSocket:function(){
 
         app.socket = io.connect('/?token='+token);
@@ -34,3 +59,4 @@ var app = {
 };
 
 app.connectSocket();
+app.detectMotion();
